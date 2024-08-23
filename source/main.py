1ae65b6d5a355f114cc71e8cd4fdcc1e4c6bf24c
Eduarda Tybusch <eduarda.tybusch@arcotech.io>
2024-08-23 19:38:00 -0300

    formatação do menu

--- source/main.py
+++ source/main.py
@@ -2,13 +2,16 @@ from view import gerenciar_estudantes, gerenciar_professores, gerenciar_discipli
 
 def menu_principal():
   while True:
-    print("Menu Principal")
+    print("\n" + "="*30)
+    print("          Menu Principal          ")
+    print("="*30)
     print("1: Gerenciar estudantes")
     print("2: Gerenciar professores")
     print("3: Gerenciar disciplinas")
     print("4: Gerenciar turmas")
     print("5: Gerenciar matrículas")
     print("9: Sair")
+    print("="*30 + "\n")
 
     opcao = input("Digite a opção desejada: ")
 
