7de1dd83906d1a46a9149a9a2ce5329ed77bfc4f
devdudd <tybuscheduarda@gmail.com>
2024-09-09 20:16:30 -0300

    refac(*): formatando código com black formatter

--- src/controller/estudante_controller.py
+++ src/controller/estudante_controller.py
@@ -1,16 +1,14 @@
 estudantesArray = []
 
+
 def criar_estudante():
     codigo = int(input("Digite o código do estudante: "))
     nome = input("Digite o nome do estudante: ")
     cpf = input("Digite o CPF do estudante: ")
-    estudante = {
-      "codigo": codigo,
-      "nome": nome,
-      "cpf": cpf
-    }
+    estudante = {"codigo": codigo, "nome": nome, "cpf": cpf}
     estudantesArray.append(estudante)
     print("\nEstudante criado!")
+
 
 def ler_estudante():
     if not estudantesArray:
@@ -18,26 +16,30 @@ def ler_estudante():
     else:
         print("\nLista de estudantes:")
         for estudante in estudantesArray:
-            print(f"Código: {estudante['codigo']}, Nome: {estudante['nome']}, CPF: {estudante['cpf']}")
+            print(
+                f"Código: {estudante['codigo']}, Nome: {estudante['nome']}, CPF: {estudante['cpf']}"
+            )
+
 
 def atualizar_estudante():
     codigo_de_entrada = int(input("Digite o código do estudante a ser atualizado: "))
     for estudante in estudantesArray:
-        if estudante['codigo'] == codigo_de_entrada:
+        if estudante["codigo"] == codigo_de_entrada:
             novo_codigo = int(input("Digite o novo código do estudante: "))
             novo_nome = input("Digite o novo nome do estudante: ")
             nove_cpf = input("Digite o novo CPF do estudante: ")
-            estudante['codigo'] = novo_codigo
-            estudante['nome'] = novo_nome
-            estudante['cpf'] = nove_cpf
+            estudante["codigo"] = novo_codigo
+            estudante["nome"] = novo_nome
+            estudante["cpf"] = nove_cpf
             print("\nEstudante atualizado!")
             return
     print("\nEstudante não encontrado!")
 
+
 def deletar_estudante():
     codigo_de_entrada = int(input("Digite o código do estudante a ser deletado: "))
     for estudante in estudantesArray:
-        if estudante['codigo'] == codigo_de_entrada:
+        if estudante["codigo"] == codigo_de_entrada:
             estudantesArray.remove(estudante)
             print("\nEstudante deletado!")
             return
