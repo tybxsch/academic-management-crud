2cabac0ec358ba0ffebc822b7e8c41207353f5b0
devdudd <tybuscheduarda@gmail.com>
2024-09-09 20:09:03 -0300

    feat(estudante): lógica das ações do CRUD

--- src/controller/estudante_controller.py
+++ src/controller/estudante_controller.py
@@ -1,23 +1,44 @@
-estudantes = []
+estudantesArray = []
+
 def criar_estudante():
-  # Lógica para criar um estudante
-  nome = input("Digite o nome do estudante: ")
-  estudantes.append(nome)
-  print("\nEstudante criado!")
+    codigo = int(input("Digite o código do estudante: "))
+    nome = input("Digite o nome do estudante: ")
+    cpf = input("Digite o CPF do estudante: ")
+    estudante = {
+      "codigo": codigo,
+      "nome": nome,
+      "cpf": cpf
+    }
+    estudantesArray.append(estudante)
+    print("\nEstudante criado!")
 
 def ler_estudante():
-  # Lógica para ler um estudante
-  if not estudantes:
-    print("\nNão há estudantes cadastrados")
-  else: 
-    print("\nLista de estudantes:")
-    for estudante in estudantes:
-      print(estudante)
+    if not estudantesArray:
+        print("\nNão há estudantes cadastrados")
+    else:
+        print("\nLista de estudantes:")
+        for estudante in estudantesArray:
+            print(f"Código: {estudante['codigo']}, Nome: {estudante['nome']}, CPF: {estudante['cpf']}")
 
 def atualizar_estudante():
-  # Lógica para atualizar um estudante
-  print("\nEM DESENVOLVIMENTO")
+    codigo_de_entrada = int(input("Digite o código do estudante a ser atualizado: "))
+    for estudante in estudantesArray:
+        if estudante['codigo'] == codigo_de_entrada:
+            novo_codigo = int(input("Digite o novo código do estudante: "))
+            novo_nome = input("Digite o novo nome do estudante: ")
+            nove_cpf = input("Digite o novo CPF do estudante: ")
+            estudante['codigo'] = novo_codigo
+            estudante['nome'] = novo_nome
+            estudante['cpf'] = nove_cpf
+            print("\nEstudante atualizado!")
+            return
+    print("\nEstudante não encontrado!")
 
 def deletar_estudante():
-  # Lógica para deletar um estudante
-  print("\nEM DESENVOLVIMENTO")
+    codigo_de_entrada = int(input("Digite o código do estudante a ser deletado: "))
+    for estudante in estudantesArray:
+        if estudante['codigo'] == codigo_de_entrada:
+            estudantesArray.remove(estudante)
+            print("\nEstudante deletado!")
+            return
+    print("\nEstudante não encontrado!")
